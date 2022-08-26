cfd110bbeb21b62e43d42349573daa0e898939f5
Cyrus Li <cyrus.li@thetradedesk.com>
2022-08-26 12:57:27 +0800

    homepage3.0-dev fixed root url setting

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App = () => (
     <div className="app">
         <Decoration/>
         <Routes>
-            <Route path='/Home' element={ <Home/> }/>
+            <Route path='/' element={ <Home/> }/>
             <Route path='/*' element={ <Navigate to='/Home'/> }/>
         </Routes>
     </div>
